4d128bf2d13c1e23ea9a801044983c507fd3f560
Grisha1232 <58052549+Grisha1232@users.noreply.github.com>
2022-11-26 02:25:18 +0300

    Update main.c

--- C code/main.c
+++ C code/main.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
 
 
 long double realTh(double x) {
     return tanh(x);
 }
 
-long double checkPresion(long double x, long double curr) {
+long double checkPrecision(long double x, long double curr) {
     long double result = (100 - (curr * 100 / x));
     return  result > 0 ? result : result * -1;
 }
@@ -30,9 +31,24 @@ int main(int argc, char *argv[]) {
             long double result = th(x);
             printf("result for th(x) = %Lf\n", result);
             printf("result for real th(x) = %Lf\n", realTh(x));
-            printf("precision: %Lf\n\n", checkPresion(realTh(x), result));
+            printf("precision: %Lf\n\n", checkPrecision(realTh(x), result));
         }
     } else if (argc == 2) {
+        if (strcmp(argv[1], "measuring") == 0) {
+            double x = 1.39;
+            printf("Testing with x = %f\n", x);
+            long double result;
+            time_t t_start = clock();
+            for (int i = 0; i < 2000000; i++) {
+                result = th(x);
+            }
+            time_t t_end = clock();
+            printf("result for th(x) = %Lf\n", result);
+            printf("precision: %Lf\n\n", checkPrecision(realTh(x), result));
+            printf("\nThe task is done 2000000 times in %f ms\n", (difftime(t_end, t_start)) / 1000);
+            return 0;
+        }
+        double pi = 3.1415926535;
         // Get for x (argv[1])
         /* Convert the provided value to a double */
         char* eptr;
@@ -44,10 +60,14 @@ int main(int argc, char *argv[]) {
             if (errno == ERANGE)
                 printf("The value provided was out of range\n");
         }
+        if (x < -pi/2 || x > pi/2) {
+            printf("Incorrect value given\n");
+            return 1;
+        }
         long double result = th(x);
         printf("result for th(x) = %Lf\n", result);
         printf("result for real th(x) = %Lf\n", realTh(x));
-        printf("precision: %Lf\n\n", checkPresion(realTh(x), result));
+        printf("precision: %Lf\n\n", checkPrecision(realTh(x), result));
     } else if (argc == 3) {
         // Get from file all Xies and out results to another file
         FILE* input;
@@ -68,7 +88,7 @@ int main(int argc, char *argv[]) {
             long double result = th(x);
             fprintf(output, "result for th(x) = %Lf\n", result);
             fprintf(output, "result for real th(x) = %Lf\n", realTh(x));
-            fprintf(output, "precision: %Lf\n\n", checkPresion(realTh(x), result));
+            fprintf(output, "precision: %Lf\n\n", checkPrecision(realTh(x), result));
         }
         fclose(input);
         fclose(output);
